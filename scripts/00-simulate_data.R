ba009eb1206d2fbb0a37722f2edfbe9b4b2431b9
Lexi Knight <lexi.knight@mail.utoronto.ca>
2024-03-28 15:16:07 -0700

    simulated the data and started writing some tests

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -10,24 +10,69 @@
 
 #### Workspace setup ####
 # install packages 
-# install.packages("tidyverse")
+install.packages("tidyverse")
+install.packages("conflicted")
+install.packages("dplyr")
+install.packages("magrittr")
 
 # load packages
 library(tidyverse)
+library(conflicted)
 library(testthat)
-
+library(dplyr)
+library(magrittr)
 
 
 #### Simulate data ####
 
+# Set seed for reproducibility
+set.seed(0)
 
-data <- 
-  tibble( 
-    level = rnorm(n = 1000, mean = 100, sd = 10) |> floor()
-    weakness = sample(x = c("fire", "not fire"), size = 1000, replace = TRUE)
-    
-    
-    
-    
-    
-    
+# Number of entries
+num_entries <- 981
+
+# Simulating the data 
+simulated_data <- data.frame(
+  ajcc_pathologic_n = sample(c("N0", "N1", "N2", "NX"), num_entries, replace = TRUE),
+  ajcc_pathologic_m = sample(c("M0", "MX", "M1a", "M1b"), num_entries, replace = TRUE),
+  ajcc_pathologic_t = sample(c("T1", "T1a", "T1b", "T2", "T2a", "T2b", "T3", "T4", "TX"), num_entries, replace = TRUE),
+  age_at_diagnosis = round(runif(num_entries, min = 14681, max = 32872)),
+  days_to_death = round(runif(num_entries, min = 0, max = 6236)),
+  race = sample(c("not reported", "white", "american indian or alaska native", "asian", "black or african american"), num_entries, replace = TRUE),
+  ajcc_pathologic_stage = sample(c("Stage IA", "Stage IIA", "Stage IIIA", "Stage IB", "Stage IIB", "Stage IV", "Stage I"), num_entries, replace = TRUE),
+  treatment_type = sample(c("Pharmaceutical Therapy, NOS", "Radiation Therapy, NOS"), num_entries, replace = TRUE),
+  treatment = sample(c("yes", "no", "not reported"), num_entries, replace = TRUE)
+) 
+
+
+# Renaming variables for clarity - not sure if i should do this here or later ****
+simulated_data <- simulated_data %>%
+  rename(
+    Pathogenic_Stage = ajcc_pathologic_stage,
+    Lymph_Node_Involvement = ajcc_pathologic_n,
+    Presence_of_Metastasis = ajcc_pathologic_m,
+    Tumor_Size = ajcc_pathologic_t,
+    Age_at_Diagnosis_in_Days = age_at_diagnosis,
+    Days_to_Death = days_to_death,
+    Race = race,
+    Treatment_Type = treatment_type,
+    Treatment_Decision = treatment
+  )
+
+
+
+simulated_data <- simulated_data |> select( Pathogenic_Stage,
+                                            Lymph_Node_Involvement,
+                                            Presence_of_Metastasis, Tumor_Size,
+                                            Age_at_Diagnosis_in_Days,
+                                            Days_to_Death, Race, Treatment_Type,
+                                            Treatment_Decision)
+
+
+
+# View first few rows of the simulated data
+head(simulated_data)
+
+# Show a summary of the data
+summary(simulated_data)
+
--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,8 +1,8 @@
 #### Preamble ####
 # Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Lexi Knight
+# Date: 28 March 2024
+# Contact: lexi.knight@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
@@ -10,6 +10,59 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Test data ####
+
+
+
+# Testing the simulated table - * i think i should just move this to the testing file ****
+
+# Test if the dataset has 981 entries 
+test_that("Dataset has 981 entries", {
+  expect_equal(nrow(simualted_data), 981)
+})
+
+# Test if 'Pathogenic_Stage' contains the correct categories 
+test_that("Pathogenic stage variable is correct", {
+  expect_true(all(simulated_data$Pathogenic_Stage %in% c('Stage IA', 'Stage IIA', 'Stage IIIA', 'Stage IB', 'Stage IIB', 'Stage IV', 'Stage I')))
+})
+
+# Test if 'Lymph_Node_Involvement' contains the correct categories 
+test_that("Lymph node involvement variable is correct", {
+  expect_true(all(simulated_data$Lymph_Node_Involvement %in% c('N0', 'N1', 'N2', 'NX')))
+})
+
+# Test if 'Presence_of_Metastasis' contains the correct categories 
+test_that("Presence of metastasis variable is correct", {
+  expect_true(all(simulated_data$Presence_of_Metastasis %in% c('M0', 'MX', 'M1a', 'M1b')))
+})
+
+# Test if 'Tumor_Size' contains the correct categories 
+test_that("Tumor size variable is correct", {
+  expect_true(all(simulated_data$Tumor_Size %in% c('T1', 'T1a', 'T1b', 'T2', 'T2', 'T2b', 'T3', 'T4', 'TX')))
+})
+
+# Test if 'Age_at_Diagnosis_in_Days is numeric 
+class(simulated_data$Age_at_Diagnosis_in_Days) == "numeric"
+
+# Test if 'Days_to_Death' is numeric 
+class(simulated_data$Days_to_Death) == "numeric"
+
+# Test if 'Race' contains the correct categories 
+test_that("Race variable is correct", {
+  expect_true(all(simulated_data$Race %in% c('not reported", "white", "american indian or alaska native", "asian", "black or african american')))
+})
+
+# Test if 'Treatment_Type' only contains 'Pharmaceutical Therapy, NOS' and 'Radiation Therapy, NOS'
+test_that("Treatment type variable is correct", {
+  expect_true(all(simulated_dat$Treatment_Type %in% c('Pharmaceutical Therapy, NOS', 'Radiation Therapy, NOS')))
+})
+
+# Test if 'Treatment_Decision" contains the correct categories 
+test_that("Treatment decision variable is correct", {
+  expect_true(all(simulated_data$Treatment_Decision %in% c('yes', 'no', 'not reported')))
+})
+
+
+
